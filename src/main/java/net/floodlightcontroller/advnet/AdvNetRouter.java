1c5da6cad85cab8f52ba17e01b36b8e4ab51b47e
Andy Sayler <andy.sayler@gmail.com>
2013-02-28 21:08:11 -0800

    Added Frank's AdvNetRouter.java

--- src/main/java/net/floodlightcontroller/advnet/AdvNetRouter.java
+++ src/main/java/net/floodlightcontroller/advnet/AdvNetRouter.java
@@ -50,319 +50,319 @@ import org.slf4j.LoggerFactory;
 
 public class AdvNetRouter implements IFloodlightModule, IOFMessageListener {
 
-    protected static final MACAddress MAGIC = MACAddress.valueOf("00:11:00:11:00:11");
+	protected static final MACAddress MAGIC = MACAddress.valueOf("00:11:00:11:00:11");
+    // MAC addresses of hosts
+	protected static final MACAddress host1 = MACAddress.valueOf("00:00:00:00:00:01");
+	protected static final MACAddress PX = MACAddress.valueOf("00:00:00:00:00:02");
+	protected static final MACAddress host3 = MACAddress.valueOf("00:00:00:00:00:03");
+	protected static final MACAddress host4 = MACAddress.valueOf("00:00:00:00:00:04");
 
-    // EDIT HERE
-    protected static final MACAddress H1 = MACAddress.valueOf("00:00:00:00:00:01");
-    protected static final MACAddress H2 = MACAddress.valueOf("00:00:00:00:00:02");
-    protected static final MACAddress H3 = MACAddress.valueOf("00:00:00:00:00:03");
-    protected static final MACAddress PX = MACAddress.valueOf("00:00:00:00:00:04");
+	protected Map<MACAddress, SwitchPort> _mac_to_switchport;
 
-    protected Map<MACAddress, SwitchPort> _mac_to_switchport;
+	protected enum RouteMode {
+		ROUTE_DIRECT,
+		ROUTE_PROXY,
+		ROUTE_DROP,
+	};
 
-    protected enum RouteMode{
-	ROUTE_DIRECT,
-	    ROUTE_PROXY,
-	    ROUTE_DROP,
-	    };
+	protected Logger                     _log;
+	protected IRoutingService            _routingEngine;
 
-    protected Logger                     _log;
-    protected IRoutingService            _routingEngine;
+	////////////////////////////////////////////////////////////////////////////
+	//
+	// IFloodlightModule
+	//
 
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    // IFloodlightModule
-    //
+	protected IFloodlightProviderService _floodlightProvider;
 
-    protected IFloodlightProviderService _floodlightProvider;
-
-    @Override //IFloodlightModule
+	@Override //IFloodlightModule
 	public void
 	init(FloodlightModuleContext context) throws FloodlightModuleException
-    {
-	_floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
-	_routingEngine = context.getServiceImpl(IRoutingService.class);
-	_log = LoggerFactory.getLogger("AdvNetRouter");
+	{
+		_floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		_routingEngine = context.getServiceImpl(IRoutingService.class);
+		_log = LoggerFactory.getLogger("AdvNetRouter");
 		
-	_mac_to_switchport = new HashMap<MACAddress, SwitchPort>();
-    }
+		_mac_to_switchport = new HashMap<MACAddress, SwitchPort>();
+	}
 
-    @Override //IFloodlightModule
+	@Override //IFloodlightModule
 	public void startUp(FloodlightModuleContext context)
-    {
-	_floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
-    }
+	{
+		_floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+	}
 
-    @Override //IFloodlightModule
+	@Override //IFloodlightModule
 	public String
 	getName()
-    {
-	return "AdvNetRouter";
-    }
+	{
+		return "AdvNetRouter";
+	}
 
-    @Override //IFloodlightModule
+	@Override //IFloodlightModule
 	public Collection<Class<? extends IFloodlightService>>
-					  getModuleDependencies()
-    {
-	Collection<Class<? extends IFloodlightService>> l =
-	    new ArrayList<Class<? extends IFloodlightService>>();
-	l.add(IFloodlightProviderService.class);
-	return l;
-    }
+	getModuleDependencies()
+	{
+		Collection<Class<? extends IFloodlightService>> l =
+			new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		return l;
+	}
 
-    @Override //IFloodlightModule
+	@Override //IFloodlightModule
 	public boolean
 	isCallbackOrderingPrereq(OFType type, String name)
-    {
-	return false;
-    }
+	{
+		return false;
+	}
 
-    @Override //IFloodlightModule
+	@Override //IFloodlightModule
 	public boolean
 	isCallbackOrderingPostreq(OFType type, String name)
-    {
-	return false;
-    }
+	{
+		return false;
+	}
 
-    @Override //IFloodlightModule
+	@Override //IFloodlightModule
 	public Collection<Class<? extends IFloodlightService>> 
-					  getModuleServices()
-    {
-	return null;
-    }
+	getModuleServices()
+	{
+		return null;
+	}
 
-    @Override //IFloodlightModule
+	@Override //IFloodlightModule
 	public Map<Class<? extends IFloodlightService>, IFloodlightService>
-				   getServiceImpls()
-    {
-	return null;
-    }
+	getServiceImpls()
+	{
+		return null;
+	}
 
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    // IOFMessageListener
-    //
+	////////////////////////////////////////////////////////////////////////////
+	//
+	// IOFMessageListener
+	//
 
-    @Override //IOFMessageListener
+	@Override //IOFMessageListener
 	public Command
 	receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx)
-    {
-	OFPacketIn pi;
-	Ethernet   pkt;
+	{
+		_log.info("Packet received - Processing!");
+		
+		OFPacketIn pi;
+		Ethernet   pkt;
 
-	if (msg.getType() != OFType.PACKET_IN) {
-	    return Command.CONTINUE;
+		if (msg.getType() != OFType.PACKET_IN) {
+			return Command.CONTINUE;
+		}
+
+		pi  = (OFPacketIn) msg;
+		pkt = IFloodlightProviderService.bcStore.get(cntx,
+						IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+
+		IPacket p = pkt.getPayload();
+		if ( ! (p instanceof IPv4)) {
+			return Command.CONTINUE;
+		}
+
+		int        in_port = pi.getInPort();
+		int        bufid   = pi.getBufferId();
+		MACAddress dl_src  = pkt.getSourceMAC();
+		MACAddress dl_dst  = pkt.getDestinationMAC();
+
+		if (dl_dst.equals(MAGIC)) {
+
+			SwitchPort tmp = new SwitchPort(sw.getId(), in_port);
+			_mac_to_switchport.put(dl_src, tmp);
+			send_drop_rule(tmp, bufid, dl_src, dl_dst);
+			return Command.STOP;
+		}
+
+		process_pkt(sw, in_port, bufid, dl_src, dl_dst);
+		return Command.STOP;
 	}
 
-	pi  = (OFPacketIn) msg;
-	pkt = IFloodlightProviderService.bcStore.get(cntx,
-						     IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-
-	IPacket p = pkt.getPayload();
-	if ( ! (p instanceof IPv4)) {
-	    return Command.CONTINUE;
-	}
-
-	int        in_port = pi.getInPort();
-	int        bufid   = pi.getBufferId();
-	MACAddress dl_src  = pkt.getSourceMAC();
-	MACAddress dl_dst  = pkt.getDestinationMAC();
-
-	if (dl_dst.equals(MAGIC)) {
-
-	    SwitchPort tmp = new SwitchPort(sw.getId(), in_port);
-	    _mac_to_switchport.put(dl_src, tmp);
-	    send_drop_rule(tmp, bufid, dl_src, dl_dst);
-	    return Command.STOP;
-	}
-
-	process_pkt(sw, in_port, bufid, dl_src, dl_dst);
-	return Command.STOP;
-    }
-
-    private void
+	private void
 	process_pkt(IOFSwitch sw, int in_port, int bufid, MACAddress dl_src, MACAddress dl_dst)
-    {
-	RouteMode  rm;
-	SwitchPort sp_src, sp_dst, sp_prx;
+	{
+		RouteMode  rm;
+		SwitchPort sp_src, sp_dst, sp_prx;
 
-	_log.debug("packet_in: " +
-		   sw.getId() + ":" + in_port + " " +
-		   dl_src + " --> " + dl_dst
-		   );
+		_log.debug("packet_in: " +
+			sw.getId() + ":" + in_port + " " +
+			dl_src + " --> " + dl_dst
+		);
 
-	sp_src = _mac_to_switchport.get(dl_src);
-	sp_dst = _mac_to_switchport.get(dl_dst);
-	sp_prx = _mac_to_switchport.get(PX);
+		sp_src = _mac_to_switchport.get(dl_src);
+		sp_dst = _mac_to_switchport.get(dl_dst);
+		sp_prx = _mac_to_switchport.get(PX);
 
-	if (sp_src == null) {
-	    _log.error("unknown source port");
-	    return;
-	} else if (sp_dst == null) {
-	    _log.error("unknown dest port");
-	    return;
-	} else if (sp_prx == null) {
-	    _log.error("unknown proxy port");
-	    return;
+		if (sp_src == null) {
+			_log.error("unknown source port");
+			return;
+		} else if (sp_dst == null) {
+			_log.error("unknown dest port");
+			return;
+		} else if (sp_prx == null) {
+			_log.error("unknown proxy port");
+			return;
+		}
+
+		rm = getCommMode(dl_src, dl_dst);
+		_log.info("packet_in: routing mode: " + rm);
+
+		if (rm == RouteMode.ROUTE_DROP) {
+
+			send_drop_rule(sp_src, bufid, dl_src, dl_dst);
+
+		} else if (rm == RouteMode.ROUTE_PROXY) {
+			create_route(sp_src, sp_prx, dl_src, dl_dst, OFPacketOut.BUFFER_ID_NONE);
+			create_route(sp_prx, sp_dst, dl_src, dl_dst, OFPacketOut.BUFFER_ID_NONE);
+			create_route(sp_dst, sp_prx, dl_dst, dl_src, OFPacketOut.BUFFER_ID_NONE);
+			create_route(sp_prx, sp_src, dl_dst, dl_src, bufid);
+
+		} else { // ROUTE_DIRECT
+
+			create_route(sp_src, sp_dst, dl_src, dl_dst, OFPacketOut.BUFFER_ID_NONE);
+			create_route(sp_dst, sp_src, dl_dst, dl_src, bufid);
+		}
 	}
 
-	rm = getCommMode(dl_src, dl_dst);
-	_log.info("packet_in: routing mode: " + rm);
-
-	if (rm == RouteMode.ROUTE_DROP) {
-
-	    send_drop_rule(sp_src, bufid, dl_src, dl_dst);
-
-	} else if (rm == RouteMode.ROUTE_PROXY) {
-
-	    create_route(sp_src, sp_prx, dl_src, dl_dst, OFPacketOut.BUFFER_ID_NONE);
-	    create_route(sp_prx, sp_dst, dl_src, dl_dst, OFPacketOut.BUFFER_ID_NONE);
-	    create_route(sp_dst, sp_prx, dl_dst, dl_src, OFPacketOut.BUFFER_ID_NONE);
-	    create_route(sp_prx, sp_src, dl_dst, dl_src, bufid);
-
-	} else { // ROUTE_DIRECT
-
-	    create_route(sp_src, sp_dst, dl_src, dl_dst, OFPacketOut.BUFFER_ID_NONE);
-	    create_route(sp_dst, sp_src, dl_dst, dl_src, bufid);
-	}
-    }
-
-    private RouteMode
+	private RouteMode
 	getCommMode(MACAddress src, MACAddress dst)
-    {
-	// EDIT HERE
-	// H1 <--> H2 : Drop
-	if((src.equals(H1) && dst.equals(H2)) || 
-	   (src.equals(H2) && dst.equals(H1))){
-	    _log.info("pair: H1 <--> H2 : Drop");
-	    return RouteMode.ROUTE_DROP;
-	}
-	// H1 <--> H3 : Direct
-	else if((src.equals(H1) && dst.equals(H3)) || 
-		(src.equals(H3) && dst.equals(H1))){
-	    _log.info("pair: H1 <--> H3 : Direct");
-	    return RouteMode.ROUTE_DIRECT;
-	}
-	// H2 <--> H3 : Proxy
-	else if((src.equals(H2) && dst.equals(H3)) || 
-		(src.equals(H3) && dst.equals(H2))){
-	    _log.info("pair: H2 <--> H3 : Proxy");
-	    return RouteMode.ROUTE_PROXY;
-	}
-	else {
-	    return RouteMode.ROUTE_DROP;
-	}
-    }
+	{
 
-    private void
+		// Hosts 1 and 3 allowed to communicate by proxy (Should be MAC Address 00:00:00:00:00:02)
+		if (src.equals(host1) && dst.equals(host3)) {
+			_log.info("Host 1 trying to connect to Host 3...");
+			return RouteMode.ROUTE_PROXY;
+		// Hosts 1 and 4 are not allowed to communicate at all (Send a ROUTE_DROP)
+		} else { 
+			if (src.equals(host1) && dst.equals(host4)) {
+				_log.info("Host 1 trying to connect to Host 4...");
+				return RouteMode.ROUTE_DROP;
+			// Hosts allowed to communicate otherwise.
+			} else {
+				_log.info("No rule, routing directly...");
+				return RouteMode.ROUTE_DIRECT; 
+			}
+		}
+	}
+
+	private void
 	create_route(
-		     SwitchPort sp_src, SwitchPort sp_dst,
-		     MACAddress dl_src, MACAddress dl_dst,
-		     int bufid)
-    {
-	Route route =
-	    _routingEngine.getRoute(
-				    sp_src.getSwitchDPID(), (short) sp_src.getPort(),
-				    sp_dst.getSwitchDPID(), (short) sp_dst.getPort(),
-				    0
-				    );
+		SwitchPort sp_src, SwitchPort sp_dst,
+		MACAddress dl_src, MACAddress dl_dst,
+		int bufid)
+	{
+		Route route =
+			_routingEngine.getRoute(
+				sp_src.getSwitchDPID(), (short) sp_src.getPort(),
+				sp_dst.getSwitchDPID(), (short) sp_dst.getPort(),
+				false
+			);
 
-	_log.info("Route: " + route);
-	List<NodePortTuple> switchPortList = route.getPath();
+		_log.info("Route: " + route);
+		List<NodePortTuple> switchPortList = route.getPath();
 
-	for (int indx = switchPortList.size()-1; indx > 0; indx -= 2) {
-	    // indx and indx-1 will always have the same switch DPID.
+		for (int indx = switchPortList.size()-1; indx > 0; indx -= 2) {
+			// indx and indx-1 will always have the same switch DPID.
 
-	    long  dpid     = switchPortList.get(indx).getNodeId();
-	    short out_port = switchPortList.get(indx).getPortId();
-	    short in_port  = switchPortList.get(indx-1).getPortId();
+			long  dpid     = switchPortList.get(indx).getNodeId();
+			short out_port = switchPortList.get(indx).getPortId();
+			short in_port  = switchPortList.get(indx-1).getPortId();
 
-	    write_flow(dpid, in_port, dl_src, dl_dst, out_port,
-		       (indx==1)?bufid:OFPacketOut.BUFFER_ID_NONE);
+			write_flow(dpid, in_port, dl_src, dl_dst, out_port,
+  		  	  (indx==1)?bufid:OFPacketOut.BUFFER_ID_NONE);
+		}
 	}
-    }
 
-    private void
+	private void
 	send_drop_rule(SwitchPort sw1, int bufid, MACAddress src, MACAddress dst)
-    {
-	write_flow(sw1.getSwitchDPID(), (short)sw1.getPort(), src, dst, (short)-1, bufid);
-    }
+	{
+		write_flow(sw1.getSwitchDPID(), (short)sw1.getPort(), src, dst, (short)-1, bufid);
+	}
 
-    private void
+	private void
 	write_flow(long dpid, short in_port, MACAddress dl_src, MACAddress dl_dst, short out_port, int bufid)
-    {
-	List<OFAction> actions = build_output_actions(out_port);
-	OFMatch        match   = build_match(in_port, dl_src, dl_dst);
-	OFFlowMod      fmod    = build_flowmod(match, actions, bufid);
-	IOFSwitch      sw      = _floodlightProvider.getSwitches().get(dpid);
+	{
+		List<OFAction> actions = build_output_actions(out_port);
+		OFMatch        match   = build_match(in_port, dl_src, dl_dst);
+		OFFlowMod      fmod    = build_flowmod(match, actions, bufid);
+		IOFSwitch      sw      = _floodlightProvider.getSwitches().get(dpid);
 
-	_log.debug("writing flow mod: " + fmod);
+		_log.debug("writing flow mod: " + fmod);
 
-	try {
-	    sw.write(fmod, null);
-	} catch (IOException e) {
-	    _log.error("error writing flow mod: " + e);
+		try {
+			if(fmod == null){
+				_log.error("fmod is null!");
+			}
+			else{
+				_log.error("fmod is not null");
+			}
+			sw.write(fmod, null);
+		} catch (IOException e) {
+			_log.error("error writing flow mod: " + e);
+		}
 	}
-    }
 
-    private List<OFAction>
+	private List<OFAction>
 	build_output_actions(short out_port)
-    {
-	List<OFAction> actions = new ArrayList<OFAction>();
+	{
+		List<OFAction> actions = new ArrayList<OFAction>();
 
-	if (out_port > 0) {
+		if (out_port > 0) {
 
-	    OFActionOutput a = new OFActionOutput(out_port);
-	    actions.add(a);
+			OFActionOutput a = new OFActionOutput(out_port);
+			actions.add(a);
+		}
+
+		return actions;
 	}
 
-	return actions;
-    }
-
-    private OFMatch
+	private OFMatch
 	build_match(short in_port, MACAddress dl_src, MACAddress dl_dst)
-    {
-	OFMatch match = new OFMatch();
-	int wc        = OFMatch.OFPFW_ALL;
+	{
+		OFMatch match = new OFMatch();
+		int wc        = OFMatch.OFPFW_ALL;
 
-	// This /should/ be setting the wildcards and such for us
-	//match.fromString("input_port=" + in_port + "," +
-	//                 "ip_src=" + IPv4.fromIPv4Address(ip_src) + "," +
-	//                 "ip_dst=" + IPv4.fromIPv4Address(ip_dst)
-	//);
+		// This /should/ be setting the wildcards and such for us
+		//match.fromString("input_port=" + in_port + "," +
+		//                 "ip_src=" + IPv4.fromIPv4Address(ip_src) + "," +
+		//                 "ip_dst=" + IPv4.fromIPv4Address(ip_dst)
+		//);
 		
-	match.setInputPort(in_port);
-	match.setDataLayerSource(dl_src.toBytes());
-	match.setDataLayerDestination(dl_dst.toBytes());
+		match.setInputPort(in_port);
+		match.setDataLayerSource(dl_src.toBytes());
+		match.setDataLayerDestination(dl_dst.toBytes());
 
-	wc &= ~OFMatch.OFPFW_IN_PORT;
-	wc &= ~OFMatch.OFPFW_DL_SRC;
-	wc &= ~OFMatch.OFPFW_DL_DST;
-	match.setWildcards(wc);
+		wc &= ~OFMatch.OFPFW_IN_PORT;
+		wc &= ~OFMatch.OFPFW_DL_SRC;
+		wc &= ~OFMatch.OFPFW_DL_DST;
+		match.setWildcards(wc);
 
-	return match;
-    }
+		return match;
+	}
 
-    private OFFlowMod
+	private OFFlowMod
 	build_flowmod(OFMatch match, List<OFAction> actions, int bufid)
-    {
-	OFFlowMod fm =
-	    (OFFlowMod) _floodlightProvider.getOFMessageFactory().getMessage(OFType.FLOW_MOD);
+	{
+		OFFlowMod fm =
+			(OFFlowMod) _floodlightProvider.getOFMessageFactory().getMessage(OFType.FLOW_MOD);
 		
-	fm.setMatch(match);
-	fm.setActions(actions);
-	fm.setBufferId(bufid);
+		fm.setMatch(match);
+		fm.setActions(actions);
+		fm.setBufferId(bufid);
 
-	int len = OFFlowMod.MINIMUM_LENGTH + OFActionOutput.MINIMUM_LENGTH * actions.size();
+		int len = OFFlowMod.MINIMUM_LENGTH + OFActionOutput.MINIMUM_LENGTH * actions.size();
 		
-	//fm.setIdleTimeout((short)0);
-	//fm.setHardTimeout((short)0);
-	fm.setCookie(AppCookie.makeCookie(100, 0));
-	fm.setCommand(OFFlowMod.OFPFC_ADD);
-	fm.setLengthU(len);
+		//fm.setIdleTimeout((short)0);
+		//fm.setHardTimeout((short)0);
+		fm.setCookie(AppCookie.makeCookie(100, 0));
+		fm.setCommand(OFFlowMod.OFPFC_ADD);
+		fm.setLengthU(len);
 
-	return fm;
-    }
+		return fm;
+	}
 }
 
 /* vim: set noet ts=4 sw=4 */
